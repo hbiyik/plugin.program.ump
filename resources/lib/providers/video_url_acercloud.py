ff3e279535716d3670d7256ea25ab06a804d5d5e
huseyinbiyik <huseyinbiyik@hotmail.com>
2015-09-06 05:06:43 +0300

    kissanime: disabled site is down, acercloud: new url provider

---
+++ resources/lib/providers/video_url_acercloud.py
@@ -0,0 +1,9 @@
+import re
+
+def run(hash,ump,referer=None):
+	iframe=ump.get_page(hash["url"],"utf-8",referer=referer)
+	files=re.findall('file: "(.*?)", label: "(.*?)"',iframe)
+	mirrors={}
+	for file in files:
+		mirrors[file[1]]=file[0]
+	return mirrors
