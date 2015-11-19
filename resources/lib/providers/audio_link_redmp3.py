ce6f9f6b93ef3effc18cbc7da7c762e689230b28
huseyinbiyik <huseyinbiyik@hotmail.com>
2015-11-19 22:39:17 +0200

    redmp3: fix album/artist referers, prevent multiple part injection

--- resources/lib/providers/audio_link_redmp3.py
+++ resources/lib/providers/audio_link_redmp3.py
@@ -39,6 +39,7 @@ def run(ump):
 	globals()['ump'] = ump
 	i=ump.info
 	if i["title"]=="" :
+		ids=[]
 		if i["album"]=="":
 			ump.add_log("redmp3 is searching artist: %s"%i["artist"])
 			#artist only
@@ -46,9 +47,10 @@ def run(ump):
 			count=0
 			for result in crawl_search(i["artist"]):
 				id,artist,title,album=result
-				if ump.is_same(artist,i["artist"]):
+				if ump.is_same(artist,i["artist"]) and not id in ids:
+					ids.append(id)
 					count+=1
-					parts.append({"url_provider_name":"redmp3", "url_provider_hash":id,"partname":artist+" - "+title})
+					parts.append({"url_provider_name":"redmp3", "url_provider_hash":id,"partname":artist+" - "+title,"referer":domain})
 			if count>0:
 				ump.add_log("redmp3 is found %d tracks for artist: %s"%(count,i["artist"]))
 				mname="[ARTIST:%s],%d Tracks" %(i["artist"],count)
@@ -61,10 +63,12 @@ def run(ump):
 			parts=[]
 			count=0
 			for result in crawl_search(i["artist"] + " - " + i["album"]):
+				print result
 				id,artist,title,album=result
-				if ump.is_same(artist,i["artist"]) and ump.is_same(album,i["album"]):
+				if ump.is_same(artist,i["artist"]) and ump.is_same(album,i["album"]) and not id in ids:
+					ids.append(id)
 					count+=1
-					parts.append({"url_provider_name":"redmp3", "url_provider_hash":id,"partname":artist+" - "+title})
+					parts.append({"url_provider_name":"redmp3", "url_provider_hash":id,"partname":artist+" - "+title,"referer":domain})
 			if count>0:
 				ump.add_log("redmp3 is found %d tracks in album: %s"%(count,i["album"]))
 				mname="[ALBUM:%s],%d Tracks" %(i["album"],count)
