45560448ba8b1f598fae1d6a590a325e7dae4ba9
huseyinbiyik <huseyinbiyik@hotmail.com>
2016-01-13 19:01:17 +0200

    vidlockers: wip, too slow server causes player to timout, novamov: wip

---
+++ resources/lib/providers/wip/!video_url_vidlockers.py
@@ -0,0 +1,16 @@
+import urlparse
+import re
+import time
+from third import unpack
+domain="http://www.vidlockers.ag/"
+def run(hash,ump,referer=None):
+	src = ump.get_page(domain+hash,"utf8",referer=referer)
+	vars=["op","id","rand","method_free","method_premium"]
+	data={}
+	for var in vars:
+		data[var]=re.findall('type="hidden" name="'+var+'" value="(.*?)"',src)[0]
+	time.sleep(1)
+	data["down_script"]="1"
+	src = ump.get_page(domain+hash,"utf8",data=data,referer=domain+hash)
+	files=re.findall('file: "(.*?)"',src)
+	return {"video":{"url":files[0],"referer":domain+hash}}
