207f5fbac41b442e6f26c089203adbde8bf9029c
huseyinbiyik <huseyinbiyik@hotmail.com>
2016-04-03 00:55:10 +0300

    grooveshark: new link provider, youtube: new url provider

---
+++ lib/providers/audio_url_youtube.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import ctypes
+from third import youtube_dl
+
+def run(hash,ump,referer=""):
+	ydl = youtube_dl.YoutubeDL({'format': 'bestaudio/best',"quiet":True})
+
+	with ydl:
+		result = ydl.extract_info(
+			'http://www.youtube.com/watch?v=%s'%hash,
+			download=False # We just want to extract the info
+		)
+
+	if 'entries' in result:
+		# Can be a playlist or a list of videos
+		audio = result['entries'][0]
+	else:
+		# Just a video
+		audio = result
+
+	return {"audio":audio["url"]}
