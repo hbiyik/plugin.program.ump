d7bf536cd1b48cebded0a324d2f65388ca7235b9
huseyinbiyik <huseyinbiyik@hotmail.com>
2016-06-12 00:27:40 +0300

    sezonlukdizi: fix search, fix video scrapers

--- lib/providers/video_link_sezonlukdizi.py
+++ lib/providers/video_link_sezonlukdizi.py
@@ -14,7 +14,7 @@ def run(ump):
 
 	for name in names:
 		ump.add_log("sezonlukdizi is searching %s"%name)
-		page=ump.get_page(domain+"/diziler.asp",encoding,query={"adi":name})
+		page=ump.get_page(domain+"/diziler.asp",encoding,query={"adi":name,"tur":"","ulke":"","tv":"","minYil":"","maxYil":"","minImdb":"","siralama_tipi":"imdb","siralama=turu":"desc","ps":"25"})
 		series=re.findall('href="(.*?)" class="header">(.*?) \(([0-9]{4})\)</a>',page)
 		for serie in series:
 			l,t,y=serie
@@ -30,24 +30,23 @@ def run(ump):
 				for video_id in video_ids:
 					vurl=domain+"/ajax/dataEmbed.asp"
 					vpage=ump.get_page(vurl,encoding,referer=url,data={"id":video_id},header={"X-Requested-With":"XMLHttpRequest"})
-					glink=re.findall('src="(http://sdp.sezonlukdizi.com/.*?)"',vpage)
-					uprv=""
-					if len(glink)>0:
-						videos=re.findall('file:"(.*?)", label:"(.*?)"',ump.get_page(glink[0],encoding,referer=url))
-						vlink={}
-						for video in videos:
-							vlink[video[1]]=video[0]
-						uprv="google"
-					#oklink=re.findall('<iframe src="(.*?)"',js["part"]["code"])
-					#if len(oklink)>0:
-					#	oksrc=ump.get_page(oklink[0],encoding,referer=domain)
-						#vlink=re.findall("id='\+([0-9]*?)\+'",oksrc)[0]
-					#	vlink={}
-					#	links=re.findall('file:"(.*?)", label:"(.*?)"',oksrc)
-					#	for link in links:
-					#		vlink[link[1]]=link[0]
-					#	uprv="okru"
-					if not uprv=="":
-						parts=[{"url_provider_name":uprv, "url_provider_hash":vlink,"referer":domain}]
-						ump.add_mirror(parts,"%s %dx%d %s" % (i["tvshowtitle"],i["season"],i["episode"],i["title"]))				
+					links=re.findall("src=\"(.*?)\"",vpage)
+					if len(links):
+						parts=[]
+						prefix=""
+						link=links[0]
+						if "plussd.asp" in link:
+							gpage=ump.get_page(link,encoding,referer=url)
+							gvids=re.findall('file:"(.*?)", label:"(.*?)",type:"mp4"',gpage)
+							mparts={}
+							for gvid in gvids:
+								mparts[gvid[1]]=gvid[0]
+							parts.append({"url_provider_name":"google", "url_provider_hash":mparts,"referer":url})
+						elif "openload.co" in link:
+							olink=link.split("embed/")
+							olink=olink[1].split("/")[0]
+							parts.append({"url_provider_name":"openload", "url_provider_hash":olink,"referer":url})
+							prefix="[HS:TR]"
+						if len(parts):
+							ump.add_mirror(parts,"%s%s %dx%d %s" % (prefix,i["tvshowtitle"],i["season"],i["episode"],i["title"]))
 				break
