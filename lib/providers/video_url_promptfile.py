475ae3aeab3a10e9930ba5b57cf8be10c643be89
huseyinbiyik <huseyinbiyik@hotmail.com>
2016-08-07 02:33:21 +0300

    promtfile: fixed, bring it on

--- lib/providers/video_url_promptfile.py
+++ lib/providers/video_url_promptfile.py
@@ -1,11 +1,13 @@
 import re
 
 domain="http://www.promptfile.com/"
+header={"User-Agent":"Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
 def run(hash,ump,referer=None):
-	src = ump.get_page(domain+"l/"+hash,"utf8",referer=referer)
-	validator=re.findall('name="chash" value="(.*?)"',src)
-	if len(validator)>0:
-		data={"chash":validator[0]}
-		src = ump.get_page(domain+"l/"+hash,"utf8",data=data,referer=domain+"l/"+hash)
-	files=re.findall("url: '(.*?)'.*?canvas\: \{",src,re.DOTALL)
+	src = ump.get_page(domain+"l/"+hash,"utf8",referer=referer,header=header)
+	validator=re.findall('id="chash" name  =  "(.*?)" value="(.*?)"',src)
+	if len(validator):
+		pf=re.findall('val\("(.*?)"\+',src)
+		data={validator[0][0]:pf[0]+validator[0][1]}
+		src = ump.get_page(domain+"l/"+hash,"utf8",data=data,referer=domain+"l/"+hash,header=header)
+	files=re.findall("url: '(.*?)'",src,re.DOTALL)
 	return {"video":{"url":files[0],"referer":domain+"l/"+hash}}
