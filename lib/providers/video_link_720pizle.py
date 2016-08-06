3a3ed02e15b238feda02a453deee0c4e6cdbc986
huseyinbiyik <huseyinbiyik@hotmail.com>
2016-08-07 01:32:10 +0300

    divxstage: fixed, 720pizle: ctach pcloud url providers,  pcloud: new video url provider

--- lib/providers/video_link_720pizle.py
+++ lib/providers/video_link_720pizle.py
@@ -1,6 +1,6 @@
-import json
 import re
 import time
+import urlparse
 
 
 domain="http://720pizle.com"
@@ -20,19 +20,26 @@ def crawl_movie_page(src,url,name):
 		(prv,hash)=movie_function[0]
 		if prv == "vidag":
 			prv="vid"
-		return prv,hash,res,name
+		return prv,hash,res,name,None
 	else:
 		#try plusplayer
 		hash=re.findall("class=\"plusplayer\".*?>(.*)</div",src)
 		if len(hash) > 0:
-			return "plusplayer",hash[0],res,name
-		else:
-			hash=re.findall("(http\://webteizle.org/player/vk\.asp.*?)\"",src)
-			if len(hash) > 0:
-				return "vkplayer",hash[0],res,name
-			else:
-				ump.add_log("720pizle Movie page has different encryption: %s" % str(url))
-				return None,None,None,None
+			return "plusplayer",hash[0],res,name,None
+		hash=re.findall("(http\://webteizle.org/player/vk\.asp.*?)\"",src)
+		if len(hash) > 0:
+			return "vkplayer",hash[0],res,name,None
+		hash=re.findall('<iframe src="(http://720pizle.com/player/pcloud\.asp.*?)"',src)
+		if len(hash):
+			src=ump.get_page(hash[0],encoding,referer=domain+url)
+			hash=re.findall("url: '(https://api.pcloud.com/getvideolinks.*?)'",src)
+			if len(hash):
+				hash=urlparse.urlparse(hash[0])
+				hash=urlparse.parse_qs(hash.query)
+				return "pcloud",hash.get("fileid",[''])[0],res,name,hash.get("auth",[''])[0]
+			hash=""
+		ump.add_log("720pizle Movie page has different encryption: %s" % str(url))
+		return None,None,None,None,None
 
 def return_links(name,type,upname,uphash,res,referer):
 	if not upname is None:
@@ -55,7 +62,6 @@ def run(ump):
 		ump.add_log("720pizle is searching %s" % name)
 		results=ump.get_page("%s/ara.asp"%domain,encoding,query={"a":name})
 		results=re.findall('<span class="oval">(.*?)</span>.*?<a class="sayfa\-icerik\-baslik" href="(.*?)">',results,re.DOTALL)
-		print results
 		#results=json.loads(ump.get_page(domain+"/api/autocompletesearch.asp",encoding,query={"limit":"10","q":name}))
 		#if len(results)==0 or len(results)==1 and "orgfilmadi" in results[0].keys() and "Bulunamad" in results[0]["orgfilmadi"] : 
 		#	ump.add_log("720pizle can't find any links for %s"%name)
@@ -101,8 +107,9 @@ def run(ump):
 	for movie_page in movie_pages:
 		movie_page_type=["dub","sub"][movie_page.split("/")[2]=="altyazi"]
 		src=ump.get_page(domain+movie_page,encoding)
-		prv,hash,res,name2=crawl_movie_page(src,movie_page,name)
-		return_links(name2,movie_page_type,prv,hash,res,domain+movie_page)
+		prv,hash,res,name2,ref=crawl_movie_page(src,movie_page,name)
+		if ref is None:	ref=domain+movie_page
+		return_links(name2,movie_page_type,prv,hash,res,ref)
 		alts=[]
 		urls = re.findall('href="(/izle/.*?)" rel="nofollow"(.*?)</a>',src)
 		alts=[x[0] for x in urls if not "tlb_isik" in x[1] ]
@@ -111,10 +118,8 @@ def run(ump):
 		for alt in alts:
 			src=ump.get_page(domain+alt,encoding)
 			#ump.add_log("720pizle is crawling %s" % alt)
-			prv,hash,res,name2=crawl_movie_page(src,alt,name)
-			return_links(name2,movie_page_type,prv,hash,res,domain+alt)
+			prv,hash,res,name2,ref=crawl_movie_page(src,alt,name)
+			if ref is None:	ref=domain+movie_page
+			return_links(name2,movie_page_type,prv,hash,res,ref)
 	ump.add_log("720pizle finished crawling %d mirrors"%count)
-	return None
-	
-	
 	return None
--- lib/providers/video_url_divxstage.py
+++ lib/providers/video_url_divxstage.py
@@ -3,7 +3,9 @@ import urlparse
 
 
 def run(hash,ump,referer=None):
-	src=ump.get_page("http://www.divxstage.to/video/"+hash,"utf-8")
+	src=ump.get_page("http://www.cloudtime.to/video/"+hash,"utf-8")
+	sk=re.findall('<input type="hidden" name="stepkey" value="(.*?)">',src)[0]
+	src=ump.get_page("http://www.cloudtime.to/video/"+hash,"utf-8",data={"stepkey":sk,"submit":"submit"},referer="http://www.cloudtime.to/video/"+hash)
 	domain=re.findall('flashvars.domain="(.*?)"',src)
 	file=re.findall('flashvars.file="(.*?)"',src)
 	key=re.findall('flashvars.filekey="(.*?)"',src)
@@ -11,9 +13,5 @@ def run(hash,ump,referer=None):
 	data={"file":file[0],"key":key}
 	src=ump.get_page(url,"utf-8",query=data)
 	dic=urlparse.parse_qs(src)
-	for key in dic.keys():
-		if not key=="url":
-			dic.pop(key)
-		else:
-			dic[key]=dic[key][0]
-	return dic
+	video={"url":dic.get("url")[0],"referer":dic.get("site_url")[0]}
+	return video
